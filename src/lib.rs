0f5c0c5e6b4e9df540eba858f66210fddcec3a54
Paul Woolcock <paul@woolcock.us>
2014-11-11 22:50:15 -0500

    start some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-#[test]
-fn it_works() {
-}
+//! # Supervisor tree library for Rust
+//!
+//! This library contains building blocks for creating supervisor trees
+//! for supervising tasks, a la Erlang/OTP
+//!
+
+#![crate_type = "lib"]
+
+#![feature(phase, globs)]
+
+#[phase(plugin)] extern crate rest_easy;
+
+pub mod supervisor;
+// mod server;
+// mod event;
+// mod fsm;
